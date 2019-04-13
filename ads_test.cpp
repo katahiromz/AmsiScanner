df319b727c9ad0cc2373586484bcc4ab2e946a28
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-04-13 21:42:49 +0900

    improve ads_test.cpp

--- ads_test.cpp
+++ ads_test.cpp
@@ -13,7 +13,10 @@ int wmain(int argc, WCHAR **wargv)
 
         for (size_t i = 0; i < entries.size(); ++i)
         {
-            printf("%d: %ls\n", int(i), entries[i].name.c_str());
+            std::string data;
+            hr = get_ads_file(L"ads_test.cpp", entries[i], data);
+
+            printf("%d: %ls: %s\n", int(i), entries[i].name.c_str(), data.c_str());
         }
 
         return 0;
@@ -22,11 +25,17 @@ int wmain(int argc, WCHAR **wargv)
     if (argc == 2)
     {
         ADS_ENTRY entry;
-        entry.name = L"TEST";
+        entry.name = wargv[1];
+        if (entry.name[0] != L':')
+        {
+            printf("ERROR\n");
+            return -1;
+        }
 
         std::string data;
         hr = get_ads_file(L"ads_test.cpp", entry, data);
         printf("%s\n", data.c_str());
+
         return hr;
     }
 
@@ -34,6 +43,12 @@ int wmain(int argc, WCHAR **wargv)
     {
         ADS_ENTRY entry;
         entry.name = wargv[1];
+        if (entry.name[0] != L':')
+        {
+            printf("ERROR\n");
+            return -1;
+        }
+
         hr = put_ads_file(L"ads_test.cpp", entry, "TESTTEST");
         return hr;
     }
