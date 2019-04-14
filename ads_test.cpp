349102bf6c545e151299eb41c98f1f46ecaea8a7
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-04-14 09:47:28 +0900

    improve ads_tset

--- ads_test.cpp
+++ ads_test.cpp
@@ -10,26 +10,25 @@ int wmain(int argc, WCHAR **wargv)
 {
     HRESULT hr;
 
-    if (argc <= 1)
+    if (argc == 2)
     {
         std::vector<ADS_ENTRY> entries;
-        hr = get_ads_entries(L"ads_test.cpp", entries);
+        hr = get_ads_entries(wargv[1], entries);
 
         for (size_t i = 0; i < entries.size(); ++i)
         {
             std::string data;
-            hr = get_ads_file(L"ads_test.cpp", entries[i], data);
-
+            hr = get_ads_file(wargv[1], entries[i], data);
             printf("%d: %ls: %s\n", int(i), entries[i].name.c_str(), data.c_str());
         }
 
         return 0;
     }
 
-    if (argc == 2)
+    if (argc == 3)
     {
         ADS_ENTRY entry;
-        entry.name = wargv[1];
+        entry.name = wargv[2];
         if (entry.name[0] != L':')
         {
             printf("ERROR\n");
@@ -37,27 +36,32 @@ int wmain(int argc, WCHAR **wargv)
         }
 
         std::string data;
-        hr = get_ads_file(L"ads_test.cpp", entry, data);
+        hr = get_ads_file(wargv[1], entry, data);
         printf("%s\n", data.c_str());
 
         return hr;
     }
 
-    if (argc == 3)
+    if (argc == 4)
     {
         ADS_ENTRY entry;
-        entry.name = wargv[1];
+        entry.name = wargv[2];
         if (entry.name[0] != L':')
         {
             printf("ERROR\n");
             return -1;
         }
 
-        hr = put_ads_file(L"ads_test.cpp", entry, "TESTTEST");
+        char buf[256];
+        WideCharToMultiByte(CP_ACP, 0, wargv[3], -1, buf, 256, NULL, NULL);
+        hr = put_ads_file(wargv[1], entry, buf);
         return hr;
     }
 
-    return 1;
+    printf("Usage #1: ads_test file\n");
+    printf("Usage #2: ads_test file :name\n");
+    printf("Usage #3: ads_test file :name \"text\"\n");
+    return 0;
 }
 
 int main(int argc, char **argv)
